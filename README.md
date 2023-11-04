2227c244c42b1218427a15414e1f62839bf03205
sanjoy <sanjoylab66@gmail.com>
2023-11-04 20:29:49 +0600

    commit-1

---
+++ README.md
@@ -0,0 +1,7 @@
+#prettier
+add prettier extension
+
+#eslint
+add eslint extension
+npm install --save-dev eslint
+npm init @eslint/config
---
+++ index.js
@@ -0,0 +1,24 @@
+const fistName = 'Sanjoy';
+const last_Name = 'Roy';
+
+const obj = {
+  name: 'djfif',
+  age: 33
+};
+
+if(1 == 0){
+
+} 
+
+
+function getName(params) {
+  return 'sanjoy';
+}
+
+console.log(fistName); 
+
+/*eslint-disable*/
+var i = 9;
+
+/*eslint-enable*/
+var j = 9;
